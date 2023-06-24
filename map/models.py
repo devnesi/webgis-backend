34b764444700e8d8c7ac1da40a0e491eda5f041a
devnesi <nesilovegood@gmail.com>
2023-06-24 12:36:28 -0300

    add default value map

--- map/models.py
+++ map/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 
 class Map(models.Model):
     id_map = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=3000, blank=True, null=True)
+    name = models.CharField(max_length=3000, default='Unnamed map')
     enabled = models.BooleanField(blank=False, null=False, default=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
