5336e480b7fee797b66800b6adc609eef535bc28
devnesi <nesilovegood@gmail.com>
2023-06-24 08:57:57 -0300

    add dotenv and db connection

--- webgis/settings.py
+++ webgis/settings.py
@@ -87,8 +87,12 @@ WSGI_APPLICATION = 'webgis.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
 
