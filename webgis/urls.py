d5d914dc7431a28339ffc057c2e87ffb3ad11017
devnesi <nesilovegood@gmail.com>
2023-06-24 14:54:44 -0300

    create register route

--- webgis/urls.py
+++ webgis/urls.py
@@ -5,7 +5,7 @@ from rest_framework import routers
 from map.api.viewsets import MapViewSet
 from layer.api.viewsets import LayerViewSet
 from geometry.api.viewsets import GeometryViewSet
-
+from user.api.viewsets import UserViewSet
 if settings.DEBUG:
     router = routers.DefaultRouter()
 else:
@@ -15,6 +15,7 @@ else:
 router.register(r'maps', MapViewSet, basename='maps') 
 router.register(r'layers', LayerViewSet, basename='layers')
 router.register(r'geometries', GeometryViewSet, basename='geometries')
+router.register(r'register', UserViewSet, basename='register')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
