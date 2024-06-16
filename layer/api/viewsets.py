125cf4a2069a02dd11ed7cc4ecb7564178bdbef0
devnesi <davi_nesi@unesc.net>
2024-06-16 14:35:38 -0300

    move vt to layer viewset

--- layer/api/viewsets.py
+++ layer/api/viewsets.py
@@ -5,8 +5,10 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.authentication import TokenAuthentication, SessionAuthentication
 from map.models import Map
 from django_filters.rest_framework import DjangoFilterBackend
-from map.models import Map
 from django.shortcuts import get_object_or_404
+from django.db import connection
+from django.http import HttpResponse
+from rest_framework.decorators import action
 
 class LayerViewSet(ModelViewSet):
     serializer_class = LayerSerializer
@@ -31,4 +33,69 @@ class LayerViewSet(ModelViewSet):
     def destroy(self, request, *args, **kwargs):   
         return super().destroy(request, *args, **kwargs)
 
+    def tileToEnvelope(self, tile):
+        worldMercMax = 20037508.3427892
+        worldMercMin = -1 * worldMercMax
+        worldMercSize = worldMercMax - worldMercMin
+        worldTileSize = 2 ** tile['zoom']
+        tileMercSize = worldMercSize / worldTileSize
+        env = dict()
+        env['xmin'] = worldMercMin + tileMercSize * tile['x']
+        env['xmax'] = worldMercMin + tileMercSize * (tile['x'] + 1)
+        env['ymin'] = worldMercMax - tileMercSize * (tile['y'] + 1)
+        env['ymax'] = worldMercMax - tileMercSize * (tile['y'])
+        return env
+    
+    def envelopeToBoundsSQL(self, env):
+        DENSIFY_FACTOR = 4
+        env['segSize'] = (env['xmax'] - env['xmin'])/DENSIFY_FACTOR
+        sql_tmpl = 'ST_Segmentize(ST_MakeEnvelope({xmin}, {ymin}, {xmax}, {ymax}, 3857),{segSize})'
+        return sql_tmpl.format(**env)
+
+    def envelopeToSQL(self, env, pk):
+        tbl = dict()
+        tbl['env'] = self.envelopeToBoundsSQL(env)
+        tbl['geomColumn'] = 'geom'
+        tbl['table'] = 'maps_layers_geometries'
+        tbl['layer'] = pk
+
+        sql_tmpl = """
+            WITH 
+            bounds AS (
+                SELECT {env} AS geom, 
+                       {env}::box2d AS b2d
+            ),
+            mvtgeom AS (
+                SELECT ST_AsMVTGeom(ST_Transform(t.{geomColumn}, 3857), bounds.b2d) AS geom
+                FROM {table} t, bounds
+                WHERE ST_Intersects(t.{geomColumn}, ST_Transform(bounds.geom, 4326))
+                AND layer_id = {layer}
+            ) 
+            SELECT ST_AsMVT(mvtgeom.*) FROM mvtgeom
+        """
+        return sql_tmpl.format(**tbl)
+    
+    def queryVectorTile(self, sql):
+        with connection.cursor() as cursor:
+            cursor.execute(sql)
+            return cursor.fetchone()[0]
+    
+    @action(methods=['GET'], detail=True, url_path='vectortiles/(?P<z>\d+)/(?P<x>\d+)/(?P<y>\d+).pbf', permission_classes=[], authentication_classes=[TokenAuthentication])
+    def getVectorTile(self, request, pk, x ,y, z):
+
+        print(request.user)
+        get_object_or_404(Layer, pk=pk, map__user=request.user.pk)
+
+        tile =  {   
+                    'zoom':   int(z), 
+                    'x':      int(x), 
+                    'y':      int(y), 
+                    'format': 'pbf'
+                }
         
+        env= self.tileToEnvelope(tile)
+        sql = self.envelopeToSQL(env, pk)
+        pbf = self.queryVectorTile(sql)
+
+        return HttpResponse(content=pbf.tobytes(), content_type='application/vnd.mapbox-vector-tile')
+    
--- map/api/viewsets.py
+++ map/api/viewsets.py
@@ -4,10 +4,8 @@ from map.models import Map
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.authentication import TokenAuthentication, SessionAuthentication
 from rest_framework.response import Response
-from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
 from rest_framework.decorators import action
-from django.db import connection
 from layer.models import Layer
 from geometry.models import Geometry
 import json
@@ -33,69 +31,6 @@ class MapViewSet(ModelViewSet):
 
     def destroy(self, request, *args, **kwargs):   
         return super().destroy(request, *args, **kwargs)
-
-    def tileToEnvelope(self, tile):
-        worldMercMax = 20037508.3427892
-        worldMercMin = -1 * worldMercMax
-        worldMercSize = worldMercMax - worldMercMin
-        worldTileSize = 2 ** tile['zoom']
-        tileMercSize = worldMercSize / worldTileSize
-        env = dict()
-        env['xmin'] = worldMercMin + tileMercSize * tile['x']
-        env['xmax'] = worldMercMin + tileMercSize * (tile['x'] + 1)
-        env['ymin'] = worldMercMax - tileMercSize * (tile['y'] + 1)
-        env['ymax'] = worldMercMax - tileMercSize * (tile['y'])
-        return env
-    
-    def envelopeToBoundsSQL(self, env):
-        DENSIFY_FACTOR = 4
-        env['segSize'] = (env['xmax'] - env['xmin'])/DENSIFY_FACTOR
-        sql_tmpl = 'ST_Segmentize(ST_MakeEnvelope({xmin}, {ymin}, {xmax}, {ymax}, 3857),{segSize})'
-        return sql_tmpl.format(**env)
-
-    def envelopeToSQL(self, env):
-        tbl = dict()
-        tbl['env'] = self.envelopeToBoundsSQL(env)
-        tbl['geomColumn'] = 'geom'
-        tbl['table'] = 'maps_layers_geometries'
-
-        sql_tmpl = """
-            WITH 
-            bounds AS (
-                SELECT {env} AS geom, 
-                       {env}::box2d AS b2d
-            ),
-            mvtgeom AS (
-                SELECT ST_AsMVTGeom(ST_Transform(t.{geomColumn}, 3857), bounds.b2d) AS geom
-                FROM {table} t, bounds
-                WHERE ST_Intersects(t.{geomColumn}, ST_Transform(bounds.geom, 4326))
-            ) 
-            SELECT ST_AsMVT(mvtgeom.*) FROM mvtgeom
-        """
-        return sql_tmpl.format(**tbl)
-    
-    def queryVectorTile(self, sql):
-        with connection.cursor() as cursor:
-            cursor.execute(sql)
-            return cursor.fetchone()[0]
-    
-    @action(methods=['GET'], detail=True, url_path='vectortiles/(?P<z>\d+)/(?P<x>\d+)/(?P<y>\d+).pbf', permission_classes=[], authentication_classes=[])
-    def getVectorTile(self, request, pk, x ,y, z):
-        #get_object_or_404(Map, pk=pk, user=request.user)
-
-        tile =  {   
-                    'zoom':   int(z), 
-                    'x':      int(x), 
-                    'y':      int(y), 
-                    'format': 'pbf  '
-                }
-        
-        env= self.tileToEnvelope(tile)
-        sql = self.envelopeToSQL(env)
-        pbf = self.queryVectorTile(sql)
-
-        return HttpResponse(content=pbf.tobytes(), content_type='application/vnd.mapbox-vector-tile')
-    
 
     @action(methods=['POST'], detail=True, url_path='load')
     def loadFromGeojson(self, request, pk):
