9ce3360e21b35cc478094cb57e8c34bf4d6d7b26
devnesi <davi_nesi@unesc.net>
2024-10-27 11:17:59 -0300

    add fields on forms endpoint

--- form/api/serializers.py
+++ form/api/serializers.py
@@ -1,9 +1,12 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, SerializerMethodField
+
+from field.models import Field
+from field.api.serializers import FieldSerializer
 from form.models import Form
 
 class FormSerializer(ModelSerializer):
+    fields = FieldSerializer(many=True, read_only=True, source='field_set') 
     class Meta:
         model = Form
         fields = '__all__'
         read_only_fields = ['created_at', 'updated_at']
-        
