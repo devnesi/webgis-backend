de373f38b092f632d1d8fa67fc2260210cf151c5
devnesi <davi_nesi@unesc.net>
2024-10-27 23:21:21 -0300

    fix: fields permissions

--- form/api/viewsets.py
+++ form/api/viewsets.py
@@ -1,6 +1,6 @@
 from rest_framework.viewsets import ModelViewSet
 from form.api.serializers import FormSerializer
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.authentication import TokenAuthentication, SessionAuthentication
 from django_filters.rest_framework import DjangoFilterBackend
 from form.models import Form
@@ -13,8 +13,18 @@ class FormViewSet(ModelViewSet):
     filterset_fields = ['layer']
     enabled_methods = ['get', 'post', 'put', 'delete']
    
+    def get_permissions(self):       
+        if self.action == 'retrieve' or self.action == 'list':
+            permission_classes = [AllowAny]
+        else:
+            permission_classes = [IsAuthenticated]
+        return [permission() for permission in permission_classes]
+
     def get_queryset(self):
-        return Form.objects.filter(layer__map__user=self.request.user)
+        if self.request.user.is_authenticated:
+            return Form.objects.filter(layer__map__user=self.request.user)
+        else:
+            return Form.objects.all()
 
     def create(self, request):
         return super().create(request)  
